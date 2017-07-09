0c9224f9f8ffe38ddc0af92c83fe778d8c90fa41
Yuta Igarashi <y.iga.thunder@gmail.com>
2017-07-09 18:27:43 +0900

    update chapter4

--- chapter4/chapter4_q.js
+++ chapter4/chapter4_q.js
@@ -1,3 +1,19 @@
+/*
+
+# ゴール
+
+Appコンポーネントに状態を持たせて、その内容をMyComponent内で表示させる。
+iOSデバイスの画面上に、Chapter 4, Finished! と表示されれば完成。
+
+# 手順
+
+1. App Component の constructor() 内で、
+   this.state に { content: “Chapter 4, Finished!” } を代入する
+2. MyComponent を呼び出す際に、
+   this.state.content を content プロパティとして渡す
+
+*/
+
 import React, { Component } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { Constants } from 'expo';
@@ -5,10 +21,12 @@ import { Constants } from 'expo';
 export default class App extends Component {
   constructor() {
     super();
+    /* TODO: 1. this.state に { content: "Chapter 4, Finished!" } というオブジェクトを代入する */
   }
   render() {
     return (
       <View style={styles.container}>
+        {/* TODO: 2. Stateに入っている content を MyComponent に渡す */}
         <MyComponent content={} />
       </View>
     );
